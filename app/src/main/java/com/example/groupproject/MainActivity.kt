ef174ae7d3cf61050a9fba7aa46a6446091ce6a6
cpvp2 <cpvp2004@gmail.com>
2025-10-26 22:36:56 +0000

    Conor's Commit

--- app/src/main/java/com/example/groupproject/MainActivity.kt
+++ app/src/main/java/com/example/groupproject/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.groupproject
 
 import android.os.Bundle
+import android.widget.RadioButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -8,10 +9,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Home
@@ -22,8 +28,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -44,9 +52,13 @@ import androidx.navigation.compose.rememberNavController
 import com.example.groupproject.ui.theme.GroupProjectTheme
 import kotlin.io.encoding.Base64
 import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.ViewModel
 import com.example.backend.backend
 import com.example.backend.BErr
-
+import com.example.backend.Gender
+import com.example.backend.NutritionDiet
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 
 sealed class Screen(val route: String) {
@@ -72,6 +84,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 
 @Composable
 fun HomeScreen( modifier: Modifier = Modifier) {
@@ -116,29 +129,308 @@ fun HomeScreen( modifier: Modifier = Modifier) {
 }
 @Composable
 fun QuestionScreen( modifier: Modifier = Modifier) {
+    var name = rememberSaveable { mutableStateOf("") }
+    val Diabetic = rememberSaveable { mutableStateOf("Yes") }
+    var alcohol = rememberSaveable { mutableStateOf("") }
+    var heartRate = rememberSaveable { mutableStateOf("") }
+    var BloodOxygenLevel = rememberSaveable { mutableStateOf("")}
+    var BodyTemperature = rememberSaveable { mutableStateOf("") }
+    var Weight = rememberSaveable { mutableStateOf("") }
+    var MRI_Delay = rememberSaveable { mutableStateOf("") }
+    var Prescription = rememberSaveable { mutableStateOf("") }
+    var Age = rememberSaveable { mutableStateOf("") }
+    var EducationLevel = rememberSaveable { mutableStateOf("") }
+    var DominantHand = rememberSaveable { mutableStateOf("Right") }
+    var Gender = rememberSaveable { mutableStateOf("Male") }
+    var FamilyHistory = rememberSaveable { mutableStateOf("Yes") }
+    var APOEE4 = rememberSaveable { mutableStateOf("Yes") }
+    var PhysicalActivity = rememberSaveable { mutableStateOf("Yes") }
+    var DepressionStatus = rememberSaveable { mutableStateOf("Yes") }
+    var MedicationHistory = rememberSaveable { mutableStateOf("Yes") }
+    var NutrientDiet = rememberSaveable { mutableStateOf("Yes") }
+    var SleepQuality  = rememberSaveable { mutableStateOf("Good") }
+    var ChronicHealthConditions = rememberSaveable { mutableStateOf("Yes") }
+
+
     Box(modifier
         .fillMaxSize()
         .background(Color.White),
         contentAlignment = Alignment.Center
-    ){
-        Row( modifier = Modifier
-            .fillMaxSize()
-            .padding(
-                top = 50.dp, bottom = 100.dp,
-                start = 50.dp, end = 50.dp
-            ),
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(
+                    top = 50.dp, bottom = 100.dp,
+                    start = 50.dp, end = 50.dp
+                ),
         ) {
             Text(
                 text = "Question Screen",
                 fontSize = 36.sp,
-                color = Color.Black)
-        }
-        Row {
-            Text(text ="This is where you'll be able to answer the question for your diagnosis",
-                fontSize = 30.sp,
-                color = Color.Black)
+                color = Color.Black
+            )
+
+            Spacer(modifier = Modifier.height(60.dp))
+
+            TextField(
+                value = name.value,
+                onValueChange = { name.value = it },
+                label = { Text("Enter your name") },
+
+                )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Text("Diabetic")
+
+            RadioButton(
+                selected = Diabetic.value == "Yes",
+                onClick = { Diabetic.value = "Yes" }
+            )
+            Text("Yes")
+            RadioButton(
+                selected = Diabetic.value == "No",
+                onClick = { Diabetic.value = "No" }
+            )
+            Text("No")
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            TextField(
+                value = alcohol.value,
+                onValueChange = { alcohol.value = it },
+                label = { Text("Enter your alcohol level") },
+
+                )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            TextField(
+                value = heartRate.value,
+                onValueChange = { heartRate.value = it },
+                label = { Text("Enter your heart rate") },
+
+                )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            TextField(
+                value = BloodOxygenLevel.value,
+                onValueChange = { BloodOxygenLevel.value = it },
+                label = { Text("Enter your blood oxygen level") },
+
+                )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            TextField(
+                value = BodyTemperature.value,
+                onValueChange = { BodyTemperature.value = it },
+                label = { Text("Enter your body temperature") },
+
+                )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            TextField(
+                value = Weight.value,
+                onValueChange = { Weight.value = it },
+                label = { Text("Enter your weight") },
+
+                )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            TextField(
+                value = MRI_Delay.value,
+                onValueChange = { MRI_Delay.value = it },
+                label = { Text("Enter your MRI delay") },
+
+                )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            TextField(
+                value = Prescription.value,
+                onValueChange = { Prescription.value = it },
+                label = { Text("Enter your prescription") },
+
+                )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            TextField(
+                value = Age.value,
+                onValueChange = { Age.value = it },
+                label = { Text("Enter your age") },
+
+                )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            TextField(
+                value = EducationLevel.value,
+                onValueChange = { EducationLevel.value = it },
+                label = { Text("Enter your Education Level") },
+
+                )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Text("Dominant Hand?")
+
+            RadioButton(
+                selected = DominantHand.value == "Right",
+                onClick = { DominantHand.value = "Right" }
+            )
+            Text("Right")
+            RadioButton(
+                selected = DominantHand.value == "Left",
+                onClick = { DominantHand.value = "Left" }
+            )
+            Text("Left")
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Text("Gender?")
+
+            RadioButton(
+                selected = Gender.value == "Male",
+                onClick = { Gender.value = "Male" }
+            )
+            Text("Male")
+            RadioButton(
+                selected = Gender.value == "Female",
+                onClick = { Gender.value = "Female" }
+            )
+            Text("Female")
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Text("Family History?")
+
+            RadioButton(
+                selected = FamilyHistory.value == "Yes",
+                onClick = { FamilyHistory.value = "Yes" }
+            )
+            Text("Yes")
+            RadioButton(
+                selected = FamilyHistory.value == "No",
+                onClick = { FamilyHistory.value = "No" }
+            )
+            Text("No")
+
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Text("APOEE4?")
+
+            RadioButton(
+                selected = APOEE4.value == "Yes",
+                onClick = { APOEE4.value = "Yes" }
+            )
+            Text("Yes")
+            RadioButton(
+                selected = APOEE4.value == "No",
+                onClick = { APOEE4.value = "No" }
+            )
+            Text("No")
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Text("Physical Activity?")
+
+            RadioButton(
+                selected = PhysicalActivity.value == "Yes",
+                onClick = { PhysicalActivity.value = "Yes" }
+            )
+            Text("Yes")
+            RadioButton(
+                selected = PhysicalActivity.value == "No",
+                onClick = { PhysicalActivity.value = "No" }
+            )
+            Text("No")
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Text("Depression Status?")
+
+            RadioButton(
+                selected = DepressionStatus.value == "Yes",
+                onClick = { DepressionStatus.value = "Yes" }
+            )
+            Text("Yes")
+            RadioButton(
+                selected = DepressionStatus.value == "No",
+                onClick = { DepressionStatus.value = "No" }
+            )
+            Text("No")
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Text("Medication History?")
+
+            RadioButton(
+                selected = MedicationHistory.value == "Yes",
+                onClick = { MedicationHistory.value = "Yes" }
+            )
+            Text("Yes")
+            RadioButton(
+                selected = MedicationHistory.value == "No",
+                onClick = { MedicationHistory.value = "No" }
+            )
+            Text("No")
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Text("Nutrient Diet?")
+
+            RadioButton(
+                selected = NutrientDiet.value == "Yes",
+                onClick = { NutrientDiet.value = "Yes" }
+            )
+            Text("Yes")
+            RadioButton(
+                selected = NutrientDiet.value == "No",
+                onClick = { NutrientDiet.value = "No" }
+            )
+            Text("No")
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Text("Sleep Quality?")
+
+            RadioButton(
+                selected = SleepQuality.value == "Good",
+                onClick = { SleepQuality.value = "Good" }
+            )
+            Text("Good")
+            RadioButton(
+                selected = SleepQuality.value == "Bad",
+                onClick = { SleepQuality.value = "Good" }
+            )
+            Text("Bad")
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Text("Chronic Health Conditions?")
+
+            RadioButton(
+                selected = ChronicHealthConditions.value == "Yes",
+                onClick = { ChronicHealthConditions.value = "Yes" }
+            )
+            Text("Yes")
+            RadioButton(
+                selected = ChronicHealthConditions.value == "No",
+                onClick = { ChronicHealthConditions.value = "No" }
+            )
+            Text("No")
+
         }
     }
+
 }
 @Composable
 fun NotificationScreen( modifier: Modifier = Modifier) {
